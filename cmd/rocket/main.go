a8885692367f339511536c1f9a10dc75eca5e694
Andrej Bencic <bencicandrej94@gmail.com>
2016-10-27 22:54:44 +0200

    Moved main file into cmd/rocket

--- cmd/rocket/main.go
+++ cmd/rocket/main.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"github.com/Briareos/rocket/container"
+	"path/filepath"
 )
 
 func main() {
-	c := container.MustLoadFromPath("config.yml")
+	c := container.MustLoadFromPath(filepath.Join("..", "..", "config.yml"))
 	c.MustWarmUp()
 
 	c.HTTPServer().ListenAndServe()
